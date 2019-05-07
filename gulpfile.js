c5f46aec10674174cb1ffd38069067b03c7c4ec4
Seth Hall <seth@ushahidi.com>
2019-05-06 23:24:54 -0400

    NEW: .twig templating & partials, gulp tasks, SVG functionality
    WIP: navbar revisions

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+var gulp           = require('gulp');
+var browserSync    = require('browser-sync').create();
+var sass           = require('gulp-sass');
+var twig           = require('gulp-twig');
+var beautify       = require('gulp-jsbeautifier');
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
@@ -17,15 +19,31 @@ gulp.task('js', function() {
         .pipe(browserSync.stream());
 });
 
+// Beautify html
+gulp.task('beautify', () =>
+  gulp.src('src/*.html')
+    .pipe(beautify())
+    .pipe(gulp.dest('src'))
+);
+
+// Compile html/*.twig files into src/*.html
+gulp.task('compile', function () {
+    return gulp.src('src/html/*.twig')
+        .pipe(twig())
+        .pipe(beautify())
+        .pipe(gulp.dest('src'))
+        .pipe(browserSync.stream());
+});
+
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['sass', 'compile'], function() {
 
     browserSync.init({
         server: "./src"
     });
 
     gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch(['src/*.html', 'src/html/*.twig', 'src/templates/**/*.twig'], ['compile']);
 });
 
 gulp.task('default', ['js', 'serve']);
