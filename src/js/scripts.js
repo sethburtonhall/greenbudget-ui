f81b0c57772adf78dbfa394e823bfc17a5488a3a
Seth Hall <seth@ushahidi.com>
2019-05-14 17:53:02 -0400

    Cleans up table, adds hover and select tr highlight

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -15,4 +15,12 @@ $(document).ready(function () {
         $('.collapse.in').toggleClass('in');
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
+
+    $('#mainTable').on('click', 'tr', function(event) {
+        if($(this).hasClass('bg-highlight')){
+            $(this).removeClass('bg-highlight');
+        } else {
+            $(this).addClass('bg-highlight').siblings().removeClass('bg-highlight').parent().siblings().find('tr').removeClass('bg-highlight');
+        }
+    });
 });
